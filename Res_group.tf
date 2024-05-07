57fb743ccf87175cccda3026b4608280accc6a0a
Aditya Raj Mourya <154124179+AdityaRajMourya@users.noreply.github.com>
2024-05-07 19:40:27 +0530

    Add files via upload

---
+++ Res_group.tf
@@ -0,0 +1,15 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "westus2"
+}
